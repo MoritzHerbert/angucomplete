491553871f2cd3854ab16a3bfcfefcf7fd820be1
Moritz Herbert <moritz.herbert@gmx.de>
2015-12-24 15:03:01 +0100

    feat(lazy select): select first search result  on blur in case of exact matching

--- angucomplete.js
+++ angucomplete.js
@@ -27,7 +27,7 @@ angular.module('angucomplete', [] )
             matchClass: '@matchclass',
             searchStr: '=anguModel'
         },
-        template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" onmouseup="this.select();" ng-focus="resetHideResults()" ng-blur="hideResults()" /><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results" ng-mousedown="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
+        template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" onmouseup="this.select();" ng-focus="resetHideResults()" ng-blur="hideResults();selectResult()" /><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results" ng-mousedown="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
 
         link: function($scope, elem, attrs) {
             $scope.lastSearchTerm = null;
@@ -117,6 +117,10 @@ angular.module('angucomplete', [] )
                             originalObject: responseData[i]
                         };
 
+                        resultRow.isExactMatch = responseData[i].isExactMatch;
+
+                        delete responseData[i].isExactMatch;
+
                         $scope.results[$scope.results.length] = resultRow;
                     }
 
@@ -138,6 +142,10 @@ angular.module('angucomplete', [] )
                             for (var s = 0; s < searchFields.length; s++) {
                                 var searchfieldVal = getPropertyByPath($scope.localData[i], searchFields[s]);
                                 if(typeof searchfieldVal === 'string' && typeof str === 'string' && searchfieldVal.toLowerCase().indexOf(str.toLowerCase()) >= 0) {
+                                    if (searchfieldVal === str) {
+                                        $scope.localData[i].isExactMatch = true;
+                                    }
+
                                     matches.push($scope.localData[i]);
                                     break;
                                 }
@@ -145,6 +153,10 @@ angular.module('angucomplete', [] )
                                 if(!isNaN(searchfieldVal) && typeof str === 'string') {
                                     var haystackAsString = searchfieldVal.toString();
                                     if(haystackAsString.indexOf(str) >= 0) {
+                                        if (haystackAsString === str) {
+                                            $scope.localData[i].isExactMatch = true;
+                                        }
+
                                         matches.push($scope.localData[i]);
                                         break;
                                     }
@@ -211,6 +223,14 @@ angular.module('angucomplete', [] )
             };
 
             $scope.selectResult = function(result) {
+                if ($scope.results.length > 0 && $scope.results[0].isExactMatch) {
+                    result = $scope.results[0];
+                }
+
+                if (!result) {
+                    return;
+                }
+
                 if ($scope.matchClass) {
                     result.title = result.title.toString().replace(/(<([^>]+)>)/ig, '');
                 }
